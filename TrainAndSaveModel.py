7ec8baad8d9d2cd7ca7e4b6352292cc8eb8e745f
Zeynep <kzynp27@gmail.com>
2021-01-24 14:49:23 +0300

    initial commit

--- TrainAndSaveModel.py
+++ TrainAndSaveModel.py
@@ -22,32 +22,32 @@ if __name__ == "__main__":
                         help = "max_depth for decision tree is 2")
     args = parser.parse_args()
     
-iris = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.33, random_state=42)
+    iris = load_iris()
+    X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.33, random_state=42)
 
-with mlflow.start_run(run_name = "iris decision tree classifier") as run:
-    
-    #log model params
-    
-    maxdepth = args.maxdepth
-    
-   
-    mlflow.log_param("max_depth", maxdepth)
-    
-    
-    model = DecisionTreeClassifier(max_depth = maxdepth)
-    model.fit(X_train,y_train)
-    pred = model.predict(X_test)
+    with mlflow.start_run(run_name = "iris decision tree classifier") as run:
+        
+        #log model params
+        
+        maxdepth = args.maxdepth
+        
+       
+        mlflow.log_param("max_depth", maxdepth)
+        
+        
+        model = DecisionTreeClassifier(max_depth = maxdepth)
+        model.fit(X_train,y_train)
+        pred = model.predict(X_test)
 
-    #log model
-    
-    mlflow.sklearn.log_model(model,"decision tree model")
-    
-    #log metric
-    
-    acc = accuracy_score(y_test, pred)
-    print("Model Accuracy : {}".format(acc))
-    
-    mlflow.end_run()
+        #log model
+        
+        mlflow.sklearn.log_model(model,"decision tree model")
+        
+        #log metric
+        
+        acc = accuracy_score(y_test, pred)
+        print("Model Accuracy : {}".format(acc))
+        
+        mlflow.end_run()
     
     
